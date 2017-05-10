95472bbb0ffa1cd462cab3aba658bcc18515e2d1
Fill4 <filipe4r@gmail.com>
2017-05-11 00:48:25 +0100

    Added Figures of more runs

--- gp-tests/config_file.py
+++ gp-tests/config_file.py
@@ -10,15 +10,15 @@ fits_options['error'] = 'SAP_FLUX_ERR'
 # Settings for defining the priors for all parameters
 prior_settings = {}
 prior_settings[0] = ['Amplitude', 'uniform', 0.0, 10.0]
-prior_settings[1] = ['Timescale', 'uniform', 0.0, 0.1]
-#prior_settings[2] = ['Jitter', 'uniform', 0.0, 0.1]
+prior_settings[1] = ['Timescale', 'uniform', 0.0, 10.0]
+prior_settings[2] = ['Jitter', 'uniform', 0.0, 10.0]
 
 # Other parameters
 plot = True
 verbose = True
 
 Nmax = 1300
-module = 'celerite'
+module = 'george'
 
 nwalkers = 20
 burnin = 500
