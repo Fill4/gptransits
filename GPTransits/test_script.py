66f21a2bfd40e6a423e2820830cc3825c463aa09
Fill4 <filipe4r@gmail.com>
2017-07-06 23:57:46 +0100

    Changed folder name

---
+++ GPTransits/test_script.py
@@ -0,0 +1,36 @@
+#!/home/fill/anaconda3/bin/python
+import matplotlib.pyplot as plt
+import numpy as np
+
+timeResults = np.genfromtxt('results.dat')
+freqResults = np.genfromtxt('diamonds_results.dat')
+
+fig = plt.figure("Amplitudes_1 (S0)", figsize=(10, 7))
+plt.scatter(timeResults[:,1]*timeResults[:,2], freqResults[:,1], s=30)
+plt.title("Amplitudes_1 (S0)")
+plt.xlabel('Amplitude (ppm) Timeseries')
+plt.xlabel('Amplitude (ppm) Diamonds')
+plt.tight_layout()
+
+fig2 = plt.figure("Timescales_1 (w0)", figsize=(10, 7))
+plt.scatter(timeResults[:,2], freqResults[:,2], s=30)
+plt.title("Timescales_1 (w0)")
+plt.xlabel('Timescale (muHz) Timeseries')
+plt.xlabel('Timescale (muHz) Diamonds')
+plt.tight_layout()
+
+fig3 = plt.figure("Amplitudes_2 (S0)", figsize=(10, 7))
+plt.scatter(timeResults[:,1]*timeResults[:,2], freqResults[:,3], s=30)
+plt.title("Amplitudes_2 (S0)")
+plt.xlabel('Amplitude (ppm) Timeseries')
+plt.xlabel('Amplitude (ppm) Diamonds')
+plt.tight_layout()
+
+fig4 = plt.figure("Timescales_2 (w0)", figsize=(10, 7))
+plt.scatter(timeResults[:,2], freqResults[:,4], s=30)
+plt.title("Timescales_2 (w0)")
+plt.xlabel('Timescale (muHz) Timeseries')
+plt.xlabel('Timescale (muHz) Diamonds')
+plt.tight_layout()
+
+plt.show()
