3b38bf05d0b730a143a281841c675d80debc54ba
Fill4 <filipe4r@gmail.com>
2017-07-09 19:01:37 +0100

    Small changes

--- GPTransits/test_script.py
+++ GPTransits/test_script.py
@@ -1,36 +1,40 @@
-#!/home/fill/anaconda3/bin/python
+#!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import numpy as np
 
-timeResults = np.genfromtxt('results.dat')
-freqResults = np.genfromtxt('diamonds_results.dat')
+gpResults = np.genfromtxt('celerite_nojitter.results')
+diamondsResults = np.genfromtxt('diamonds.results')
+
+plt.rc('text', usetex=False)
 
 fig = plt.figure("Amplitudes_1 (S0)", figsize=(10, 7))
-plt.scatter(timeResults[:,1]*timeResults[:,2], freqResults[:,1], s=30)
-plt.title("Amplitudes_1 (S0)")
-plt.xlabel('Amplitude (ppm) Timeseries')
-plt.xlabel('Amplitude (ppm) Diamonds')
+plt.scatter(gpResults[:,1]*gpResults[:,2], diamondsResults[:,1], s=30)
+plt.title('Amplitude 1 S_0 (ppm)')
+plt.xlabel('GP')
+plt.ylabel('DIAMONDS')
 plt.tight_layout()
 
 fig2 = plt.figure("Timescales_1 (w0)", figsize=(10, 7))
-plt.scatter(timeResults[:,2], freqResults[:,2], s=30)
-plt.title("Timescales_1 (w0)")
-plt.xlabel('Timescale (muHz) Timeseries')
-plt.xlabel('Timescale (muHz) Diamonds')
+plt.scatter(gpResults[:,2], diamondsResults[:,2], s=30)
+plt.title("Timescale w0 (muHz)")
+plt.xlabel('GP')
+plt.ylabel('DIAMONDS')
 plt.tight_layout()
 
+'''
 fig3 = plt.figure("Amplitudes_2 (S0)", figsize=(10, 7))
-plt.scatter(timeResults[:,1]*timeResults[:,2], freqResults[:,3], s=30)
+plt.scatter(gpResults[:,1]*gpResults[:,2], diamondsResults[:,3], s=30)
 plt.title("Amplitudes_2 (S0)")
-plt.xlabel('Amplitude (ppm) Timeseries')
-plt.xlabel('Amplitude (ppm) Diamonds')
+plt.xlabel('GP')
+plt.ylabel('DIAMONDS')
 plt.tight_layout()
 
 fig4 = plt.figure("Timescales_2 (w0)", figsize=(10, 7))
-plt.scatter(timeResults[:,2], freqResults[:,4], s=30)
+plt.scatter(gpResults[:,2], diamondsResults[:,4], s=30)
 plt.title("Timescales_2 (w0)")
-plt.xlabel('Timescale (muHz) Timeseries')
-plt.xlabel('Timescale (muHz) Diamonds')
+plt.xlabel('GP')
+plt.ylabel('DIAMONDS')
 plt.tight_layout()
+'''
 
 plt.show()
