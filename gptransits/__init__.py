2ca7d6d96abdfdbdc1f0706040e85d4ec80b098f
Fill4 <filipe4r@gmail.com>
2019-05-24 14:30:14 +0100

    Removed analysis from available functions

--- gptransits/__init__.py
+++ gptransits/__init__.py
@@ -2,9 +2,6 @@
 __version__ = 0.3
 
 from .model import Model
-from.settings import Settings
-from .helper import analyse
+from .settings import Settings
 
-# Analysis is still not working with the default settings
-# Should be a class that recieves a model and analyses all the data
-__all__ = [Model, Settings, analyse]
+__all__ = [Model, Settings]
