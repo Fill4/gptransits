898287617cbf2597abcb4407b3cbfa0e521bd831
Fill4 <filipe4r@gmail.com>
2019-08-14 04:04:30 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
